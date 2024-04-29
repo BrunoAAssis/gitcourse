f4582b438e4e25fdf90aa1408ebcaef07fd07c5d
Bruno Assis <brunoaraujoassis@gmail.com>
2024-04-29 08:04:24 -0300

    SQLteste.sql

--- SQLteste.sql
+++ SQLteste.sql
@@ -1,2 +1,13 @@
 select * from tabela 
 where cpf = % 123
+order by desc 1
+select * from tabela 
+where cpf = % 123
+order by desc 1
+select * from tabela 
+where cpf = % 123
+order by desc 1
+select * from tabela 
+where cpf = % 123
+order by desc 1
+git show 152151923ef6b718912930345b3e936a684d0a20
